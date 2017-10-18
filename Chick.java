de375b92d64a75d6c692b462915cce63c31629da
mandy-wu <mandywu1031@gmail.com>
2017-10-18 11:35:51 -0700

    Old MacDonald

--- Chick.java
+++ Chick.java
@@ -1,4 +1,28 @@
-class Chick //implements Animal 
+class Chick implements Animal 
 {     
-     //your code here
+  private String myType;
+  private String mySound;
+  public Chick(String type, String sound)
+  {
+    myType = type;
+    mySound = sound;
+  }
+  public Chick()
+  {
+    myType = "Chick";
+    mySound = "cluck";
+    mySecondSound = "cheep";
+  }
+  public String getSound()
+  {
+    if (Math.random() < .5)
+     {
+      return mySound;
+     }
+    else return mySecondSound;
+  }
+  public String getType()
+  {
+    return myType;
+  }
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,14 @@
 
-void setup()
+public void setup()
 {
 	//your variable declarations here
+  Cow c = new Cow("cow", "moo");   
+  System.out.println(c.getType() + " goes " + c.getSound());
+  Chick h = new Chick("chick","cluck");  
+  System.out.println(c.getType() + " goes " + c.getSound());   
+  Pig p = new Pig("pig", "oink");   
+  System.out.println(c.getType() + " goes " + c.getSound());    
+  Farm f = new Farm();
+  f.animalSounds();
+  
 }
-
